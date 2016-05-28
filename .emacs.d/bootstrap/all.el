9ae8f1a5165264a6a3d49831cdbeb672a7e74432
MiningMarsh <miningmarsh@gmail.com>
2016-05-27 23:28:49 -0400

    Corrected comment.

--- .emacs.d/bootstrap/all.el
+++ .emacs.d/bootstrap/all.el
@@ -1,6 +1,6 @@
 ;;; all --- Library that is included for every config file.
 ;;; Commentary:
-;;; This file is NOT loaded for libraries in lib by default, they must still
+;;; This file is NOT loaded for libraries in bootstrap by default, they must still
 ;;; include packages and rc manually if they need it.
 ;;; Code:
 (require 'rc)
