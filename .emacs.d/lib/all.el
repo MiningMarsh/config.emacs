812ced770cc3e6597a0c93a5c54f4c71569e98f1
MiningMarsh <miningmarsh@gmail.com>
2016-05-27 22:56:48 -0400

    Added all library.

---
+++ .emacs.d/lib/all.el
@@ -0,0 +1,10 @@
+;;; all --- Library that is included for every config file.
+;;; Commentary:
+;;; This file is NOT loaded for libraries in lib by default, they must still
+;;; include packages and rc manually if they need it.
+;;; Code:
+(require 'rc)
+(require 'packages)
+
+(provide 'all)
+;;; all.el ends here
