0909ca999e501da455a6300977574cc8faf0fea9
Joshua Songy <joshua.songy@mailchimp.com>
2016-06-08 10:53:59 -0400

    Corrected some formatting.

--- .emacs.d/config/which-key.el
+++ .emacs.d/config/which-key.el
@@ -1,17 +1,15 @@
 ;;; which-key --- Enable which-key navigation.
 ;;; Commentary:
 ;;; Code:
-
 (packages/requires (which-key)
-	   ;; Setup which-key
-	   (which-key-setup-minibuffer)
-	   (setq which-key-popup-type 'minibuffer)
-	   (dolist (key (assoc-map "TAB" "↹"
-			       "RET" "⏎"
-			       "DEL" "⇤"
-			       "SPC" "␣"))
-	     (add-to-list 'which-key-key-replacement-alist key))
-	   (setq which-key-show-prefix 'top)
-	   (which-key-mode 1))
-
+		   ;; Setup which-key
+		   (which-key-setup-minibuffer)
+		   (setq which-key-popup-type 'minibuffer)
+		   (dolist (key (assoc-map "TAB" "↹"
+					   "RET" "⏎"
+					   "DEL" "⇤"
+					   "SPC" "␣"))
+		     (add-to-list 'which-key-key-replacement-alist key))
+		   (setq which-key-show-prefix 'top)
+		   (which-key-mode 1))
 ;;; which-key.el ends here
