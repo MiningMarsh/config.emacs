30bcc831885f2dc48e7ebbafbcc08fe421cbb9d5
Joshua Songy <joshua.songy@mailchimp.com>
2016-06-08 10:56:03 -0400

    Corrected some more formatting.

--- .emacs.d/config/flyspell.el
+++ .emacs.d/config/flyspell.el
@@ -1,7 +1,6 @@
 ;;; flyspell --- Enable spell checking for code and text documents.
 ;;; Commentary:
 ;;; Code:
-
 ;;(packages/requires (flyspell)
 ;;	   (add-hooks (prog-mode-hook)
 ;;		      (flyspell-prog-mode +1)))
