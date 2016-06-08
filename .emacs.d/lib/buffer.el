948df9ce5c78a550be2766f2ce507be7a3807bcc
Joshua Songy <joshua.songy@mailchimp.com>
2016-06-08 14:05:57 -0400

    Added buffer library.

---
+++ .emacs.d/lib/buffer.el
@@ -0,0 +1,11 @@
+;;; buffer --- Buffer library.
+;;; Commentary:
+;;; Contains extra functions for manipulating buffers.
+;;; Code:
+(packages/define buffer ()
+
+  (defun buffer/revert ()
+      "Revert the current buffer without confirmation."
+    (interactive)
+    (revert-buffer t t)))
+;;; buffer.el ends here
