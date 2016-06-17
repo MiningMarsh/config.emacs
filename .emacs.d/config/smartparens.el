de6a60dedf5dfa1a34855d60b0314c04a159a1e3
Joshua Songy <joshua.songy@mailchimp.com>
2016-06-17 15:25:14 -0400

    Started adding smartparens support.

---
+++ .emacs.d/config/smartparens.el
@@ -0,0 +1,6 @@
+;;; smartparens --- Enable smartparens.
+;;; Commentary:
+;;; Only enabled places where paredit is not already enabled.
+;;; Code:
+(packages/requires (smartparens smartparens-config))
+;;; smartparens.el ends here
