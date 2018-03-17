8e454786dec9a41aabda1f29100f3d5b8050febd
MiningMarsh <miningmarsh@gmail.com>
2018-03-17 18:30:29 -0400

    Fix broken stow ignore setup. Migrate ido configuration to non-deprecated package.

--- .emacs.d/config/ido/ido.el
+++ .emacs.d/config/ido/ido.el
@@ -2,7 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 
-(packages/requires (ido ido-ubiquitous ido-yes-or-no)
+(packages/requires (ido ido-completing-read+ ido-yes-or-no)
 		   (ido-mode 1)
 		   (ido-everywhere 1)
 		   (ido-ubiquitous-mode 1)
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,5 +2,27 @@
 ;;; Commentary:
 ;;; Load bootstrap library.
 ;;; Code:
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (load-file "~/.emacs.d/bootstrap/bootstrap.el")
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yaml-mode window-number solarized-theme spaceline spacemacs-theme rainbow-delimiters smartparens slime flymake-puppet puppet-mode ag projectile magit smex ido-yes-or-no ido-ubiquitous ido-grid-mode flx-ido flycheck evil-paredit ranger erc-yt ac-ispell erc-image column-enforce-mode key-chord elpy pretty-symbols paredit ace-jump-mode dictionary which-key evil-leader evil function-args yasnippet auto-complete achievements))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(erc-input-face ((t (:foreground "brightyellow"))))
+ '(erc-my-nick-face ((t (:foreground "brightyellow" :weight bold)))))
