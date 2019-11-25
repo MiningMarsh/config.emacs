5f3af4f1ace30cf3d165143a39189d2466ea19c7
Joshua Songy <jsongy@researchinnovations.com>
2019-11-25 09:25:40 -0500

    Improved markdown preview mode support a good bit.

--- .emacs.d/config/markdown-preview-mode.el
+++ .emacs.d/config/markdown-preview-mode.el
@@ -3,13 +3,22 @@
 ;;; Code:
 (eval-when-compile (require 'key-tree))
 
-(packages/requires (key-tree markdown-mode markdown-preview-mode)
+(packages/requires (key-tree
+		    markdown-mode
+		    markdown-preview-mode
+		    flyspell-mode
+		    flycheck-mode
+		    auto-fill-mode)
 
   (add-hooks (markdown-mode)
-	     (flyspell-mode 't))
+	     (flycheck-mode 't)
+	     (column-enforce-mode 't)
+	     (flyspell-mode 't)
+	     (auto-fill-mode 't))
 
   (key-tree/add-mode-bindings
    markdown-mode
    "m" ("Markdown Editting"
-	"p" "Open Clause" 'markdown-preview-mode)))
+	"p" "Open Clause" 'markdown-preview-mode
+	"f" "Reformat Paragraph" 'fill-paragraph)))
 ;;; markdown-preview-mode.el ends here
